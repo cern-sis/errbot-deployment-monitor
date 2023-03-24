76cd4ed2b4b5450f84a1f67c64cf21a4b2093b13
jimil749 <jimildesai42@gmail.com>
2023-03-24 08:48:09 +0100

    add alert/notification for container restarts
    Signed-off-by: jimil749 <jimildesai42@gmail.com>

--- monitor.py
+++ monitor.py
@@ -49,6 +49,54 @@ class Monitor(BotPlugin):
 
         self.log.info(response.status_code)
 
+    def _prepare_message_for_container_restart(self, pod, container_name, **kwargs):
+        BOT_API_KEY = os.environ["BOT_K8S_KEY"]
+        BOT_EMAIL = "k8s-alert-bot@cern-rcs-sis.zulipchat.com"
+        namespace = kwargs["body"]["metadata"]["namespace"]
+        if namespace not in os.environ.get("ACCEPTED_NAMESPACES", "").split(","):
+            return
+        pod_name = pod.metadata.name
+        zulip_msg_content = f"""
+                            :warning: Detected container restarted
+                            **{container_name}  has restarted in pod **{pod_name}**\n
+                            """
+        zulip_request_payload = {
+            "type": "stream",
+            "to": namespace.split("-")[0],
+            "topic": namespace,
+            "content": zulip_msg_content,
+        }
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Basic {BOT_EMAIL}:{BOT_API_KEY}",
+        }
+
+        response = requests.post(
+            "https://cern-rcs-sis.zulipchat.com/api/v1/messages",
+            json=zulip_request_payload,
+            headers=headers,
+        )
+
+        self.log.info(response.status_code)
+
+    @kopf.on.event("v1", "pods")
+    def container_restart_notification_handler(self, body, **kwargs):
+        for container_status in body.status.container_statuses:
+            if container_status.state.running and container_status.restart_count > 0:
+                container = next(
+                    (
+                        c
+                        for c in body.spec.containers
+                        if c.name == container_status.name
+                    ),
+                    None,
+                )
+                if container:
+                    self._prepare_message_for_container_restart(
+                        body, container, **kwargs
+                    )
+
     # send notification if the deployment is failing
     @kopf.on.field("apps", "v1", "deployments", field="status.replicas")
     def pod_phase_notification_handler(self, old, new, body, **kwargs):
